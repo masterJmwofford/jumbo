ea3bf842fb5b5452718e9b7d96d6abc169b84d19
Jordan Wofford <wofford.codes@gmail.com>
2024-03-25 08:43:56 -0400

    lessonplan:

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// Event Listeners
+// let button = document.querySelector('.buttonOne')
+// button.addEventListener("click", ()=> {
+//     console.log("This works")
+// })
+// Form Validation
+
+{/* <input type="text" required/> */}
+
+
+
+
+// Javascript Practice
+
+
+
+// -----------------------------------------------------beginner
+// Write a JavaScript program to reverse a given string.
+// Write a JavaScript program to capitalize the first letter of each word in a given string.    
+// Write a JavaScript program to extract the first half of a even string. 
+// Write a JavaScript program to find the closest value to 100 from two numerical values.  
+// Write a JavaScript program to check two given integers whether one is positive and another one is negative. 
+
+// Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched".  
+// Write a JavaScript program to count the number of vowels in a given string.  
+// Write a JavaScript program to check a pair of numbers and return true if one of the numbers is 50 or if their sum is 50.  
+
+// Write a JavaScript program to compute the sum of the two given integers. If the two values are the same, then return triple their sum.
+// -----------------------------------------------------medium 
+
+// Write a JavaScript program to rotate the string 'w3resource' in the right direction. This is done by periodically removing one letter from the string end and attaching it to the front.
+
+// Write a JavaScript program to remove a character at the specified position in a given string and return the modified string.
+// Write a JavaScript program that checks two integer values and returns true if either one is 15 or if their sum or difference is 15.  
+// Write a JavaScript program to create an array taking the middle elements of the two arrays 
+
+// -----------------------------------------------------advanced
+
+// Write a JavaScript program to calculate the days left before Christmas.
+
+// Write a JavaScript program to replace every character in a given string with the character following it in the alphabet.
+
+// Write a JavaScript program to convert letters of a given string alphabetically.  
